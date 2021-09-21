cfb0b91a42f0b0743349009908ac3e1cbc309060
freeNAMU <91006955+freenamu@users.noreply.github.com>
2021-09-22 01:16:31 +0900

    Separate frontend and backend

--- backend/src/main/java/com/github/freenamu/backend/repository/DocumentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.freenamu.backend.repository;
-
-import com.github.freenamu.backend.entity.Document;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DocumentRepository extends JpaRepository<Document, String> {
-}
