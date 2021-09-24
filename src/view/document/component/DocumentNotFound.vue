c2c3748aa2cf5454e0ede09ca5e06062e9e4ea0a
freeNAMU <91006955+freenamu@users.noreply.github.com>
2021-09-25 07:33:40 +0900

    Implement document not found

---
+++ src/view/document/component/DocumentNotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    문서를 찾을 수 없습니다. 문서를 생성하려면
+    <router-link :to="{name: 'DocumentEdit', params: {documentName}}">여기</router-link>
+    를 클릭하세요.
+  </section>
+</template>
+<script>
+export default {
+  name: "DocumentNotFound",
+  props: ["documentName"]
+}
+</script>
+<style scoped>
+</style>
