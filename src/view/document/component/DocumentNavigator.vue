efd570c61184636d9b1d821955a636f376a92b37
freeNAMU <freeNAMU@icloud.com>
2021-09-25 09:03:33 +0900

    Implement document history

--- src/view/document/component/DocumentNavigator.vue
+++ src/view/document/component/DocumentNavigator.vue
@@ -2,7 +2,7 @@
   <nav>
     <router-link :to="{name: 'DocumentEdit', params: {documentName, revision}}">편집</router-link>
     |
-    <router-link :to="{name: 'DocumentHistory', params: {documentName, revision}}">역사</router-link>
+    <router-link :to="{name: 'DocumentHistory', params: {documentName, revision: 'latest'}}">역사</router-link>
   </nav>
 </template>
 <script>
