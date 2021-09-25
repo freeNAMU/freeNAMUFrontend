cd2030aa0255c29bec6b046bfb96056fb4419b6d
freeNAMU <freeNAMU@icloud.com>
2021-09-25 11:07:46 +0900

    Implement search document

--- src/view/GlobalNavigator.vue
+++ src/view/GlobalNavigator.vue
@@ -1,8 +1,23 @@
 <template>
-  <nav/>
+  <nav>
+    <form @submit="searchDocument">
+      <input type="text" v-model="documentName">
+      <button type="submit">검색</button>
+    </form>
+  </nav>
 </template>
 <script>
 export default {
-  name: "GlobalNavigator"
+  name: "GlobalNavigator",
+  data () {
+    return {
+      documentName: null
+    }
+  },
+  methods: {
+    searchDocument () {
+      this.$router.push(`/view/document/${this.documentName}/latest`)
+    }
+  }
 }
 </script>
